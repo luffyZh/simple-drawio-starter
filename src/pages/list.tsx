f3af8f7ff877764b086a11f672576650b4e9e6c6
zhoudeyou <zhoudeyou@corp.netease.com>
2021-09-03 11:28:32 +0800

    :sparkles: feat: edit mode

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -8,7 +8,12 @@ export default function List() {
       status="403"
       title="个人中心页"
       subTitle="可以在这里对图表进行管理"
-      extra={<Button type="primary" onClick={() => Router.push('/')}>返回首页</Button>}
+      extra={ 
+        <>
+          <Button onClick={() => Router.push('/drawio?id=333')} danger>编辑图表</Button>
+          <Button style={{ marginLeft: 20 }} type="primary" onClick={() => Router.push('/')}>返回首页</Button>
+        </>
+      }
     />
   );
 } 
